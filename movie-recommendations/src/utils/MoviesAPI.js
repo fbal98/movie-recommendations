0119dea45f1b356eeeea13b997b3eaa0be5d2873
Loay Al Jahwari <loay.aljahwari@gmail.com>
2022-01-07 10:36:52 +0400

    added catch errors

--- movie-recommendations/src/utils/MoviesAPI.js
+++ movie-recommendations/src/utils/MoviesAPI.js
@@ -18,14 +18,14 @@ export const getGenreList = () =>
 export const getMoviesByGenre = (genre) =>
   fetch(`${api_url}/discover/movie?&with_genres=${genre}`, { headers })
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .catch((e) => console.log(e));
 
 export const getMovieKeywords = (movie_id) =>
   fetch(`${api_url}/movie/${movie_id}/keywords`, { headers })
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .catch((e) => console.log(e));
 
 export const searchForMovie = (query) =>
   fetch(`${api_url}/search/movie?&language=en-US&query=${query}`, { headers })
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .catch((e) => console.log(e));
